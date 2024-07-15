8c74212eebc21bb68268abc8a5d234ef6ee526aa
Groot2308 <danghoan2382002@gmail.com>
2024-07-15 10:44:11 +0700

    update library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ android {
 dependencies {
     implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.19")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("org.json:json:20201115")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
